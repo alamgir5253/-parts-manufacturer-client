17255e648e9dbced39ee46f6d0e2e98431a08975
alomgir hossain <alamgir5253@.com>
2022-05-21 23:44:30 +0600

    initial project packege(firebase, firebase hook, daisy ui, tailwind downloded

--- src/App.js
+++ src/App.js
@@ -3,21 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='text-center text-cyan-700'>
+      <h1>app is ok</h1>
+      <button class="btn btn-accent">Button</button>
+      
     </div>
   );
 }
