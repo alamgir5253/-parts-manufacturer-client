d4e35fe17a5a145b9fc588ff3e63373143995380
alomgir hossain <alamgir5253@.com>
2022-05-22 00:01:57 +0600

    basic route setup done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Blog from './pages/Blog/Blog';
+import Dashboard from './pages/Dashboard/Dashboard';
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login'
+import SignUp from './pages/SignUp/SignUp'
 
 function App() {
   return (
-    <div className='text-center text-cyan-700'>
-      <h1>app is ok</h1>
-      <button class="btn btn-accent">Button</button>
-      
+    <div>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/blog' element={<Blog />}></Route>
+        <Route path='/dashboard' element={<Dashboard />}></Route>
+        <Route path='/signup' element={<SignUp />}></Route>
+        <Route path='/login' element={<Login />}></Route>
+      </Routes>
     </div>
   );
 }
