6bfe25c688d8fa11a4855b65ab9d200422556129
alomgir hossain <alamgir5253@.com>
2022-06-04 00:26:46 +0600

    checking

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -6,7 +6,7 @@ const Banner = () => {
       <div className="hero-content flex-col lg:flex-row-reverse">
         <img src={banner} className="max-w-full rounded-lg " />
         <div>
-          <h1 className="md:text-4xl text-xl text-primary uppercase font-bold">We Are Hare To Serve You</h1>
+          <h1 className="md:text-4xl text-xl text-primary uppercase font-bold">We Are all Hare To Serve You</h1>
           <p className="py-6 max-w-lg text-neutral">bike parts co. is an American multinational manufacturer company. Get the best bike parts from us. we use world best technology to manufacture bike parts</p>
           <button className="btn text-white btn-primary">Get Started</button>
         </div>
